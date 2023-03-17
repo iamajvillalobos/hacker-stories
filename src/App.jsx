b73c6f1df16fbda1c30275b718f332791ead212a
AJ Villalobos <hello@ajvillalobos.com>
2023-03-18 01:51:17 +0800

    done jsx intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import * as React from "react";
 
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {getTitle("React")}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
